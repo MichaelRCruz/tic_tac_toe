266305287e1fe1cbaed83ed6e8165278171579d7
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-02 11:28:40 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+_one = document.getElementById('one');
+_two = document.getElementById('two');
+_three = document.getElementById('three');
+_four = document.getElementById('four');
+_five = document.getElementById('five');
+_six = document.getElementById('six');
+_seven = document.getElementById('seven');
+_eight = document.getElementById('eight');
+_nine = document.getElementById('nine');
+
+_one.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_two.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_three.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_four.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_five.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_six.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_seven.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_eight.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
+_nine.addEventListener('click', function() {
+  this.innerHTML = 'X';
+});
