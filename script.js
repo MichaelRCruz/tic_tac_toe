9f4f71dbb80840d52400a00fbf03e3a503a885c4
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-03 12:07:42 -0700

    replaced with hoisted functions

--- script.js
+++ script.js
@@ -9,10 +9,6 @@ eight = document.getElementById('eight');
 nine = document.getElementById('nine');
 _button = document.getElementById('clear');
 
-// for (var i = 0; i < things.length; i++) {
-//   things[i] = document.getElementById(things[i]);
-// }
-
 var board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var things = [one, two, three, four, five, six, seven, eight, nine];
 var marker = "X";
@@ -24,7 +20,7 @@ for (var i = 0; i < things.length; i++) {
   board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 });
 
-var switcher = function() {
+function switcher() {
   if (marker == "X") {
     marker = "O";
   } else if (marker == "O") {
@@ -32,7 +28,7 @@ var switcher = function() {
   }
 };
 
-var isTie = function() {
+function isTie() {
   if (board[0] !== 1 && board[1] !== 2 && board[2] !== 3 && board[3] !== 4 && board[4] !== 5 && board[5] !== 6 && board[6] !== 7 && board[7] !== 8 && board[8] !== 9) {
     alert("It's a tie!")
   } else {
@@ -40,7 +36,7 @@ var isTie = function() {
   }
 };
 
-var winLogic = function() {
+function winLogic() {
   if (board[0] == board[1] && board[0] == board[2]) {
     switcher();
     alert(marker + " wins!");
@@ -70,7 +66,7 @@ var winLogic = function() {
   }
 };
 
-var dude = function(i) {
+function dude(i) {
   things[i].addEventListener('click', function() {
     if (this.innerHTML == "X" || this.innerHTML == "O") {
       alert("This cell cannot be played again.")
@@ -86,5 +82,3 @@ var dude = function(i) {
 for (var i = 0; i < things.length; i++) {
   dude(i);
 }
-
-// this is me learning git merge
